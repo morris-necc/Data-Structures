56257ae24735eaec5ea1ce2ea1d12b37aed1d952
Morris Kim <morris.kim@binus.ac.id>
2021-02-24 11:43:53 +0700

    moved files around

--- Week 1/TA/Session 2/pointer_basic.cpp
+++ Week 1/TA/Session 2/pointer_basic.cpp
@@ -10,13 +10,14 @@ int main(){
 
 	// your code here
     int* pointer = &x;
-	cout << "The address of x is " << pointer << endl;
+	cout << "The address of x is " << pointer << endl; //print value of pointer
+	cout << "The content of x is " << *pointer << endl; //print value stored in its address
+
+	*pointer = 115; //change value stored in the address to 115
+	cout << "The address of x is " << pointer << endl; //same as above
 	cout << "The content of x is " << *pointer << endl;
 
-	*pointer = 115;
-	cout << "The address of x is " << pointer << endl;
-	cout << "The content of x is " << *pointer << endl;
-
+	//new keyword
 	int* pointer2 = new int;
 	*pointer2 = 333;
 	cout << "The value of new pointer is "<<*pointer2 << endl;
